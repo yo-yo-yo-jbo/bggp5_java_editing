681505ed3b09963542c5586e68765300b0b0c64a
Jonathan Bar Or <30564634+yo-yo-yo-jbo@users.noreply.github.com>
2024-06-29 19:32:49 -0700

    Add files via upload

--- java_fmt.py
+++ java_fmt.py
@@ -31,7 +31,7 @@ class JavaObject(object):
         Container for a class that can be freely be added attributes.
     """
 
-    def __init__(self, const_pool):
+    def __init__(self, const_pool, start_offset):
         """
             Constructor.
         """
@@ -42,35 +42,39 @@ class JavaObject(object):
         # Used for pretty-printing
         self._depth = 0
 
+        # Just tracking
+        self._start_offset = start_offset
+
     def __str__(self):
         """
             Returns a string.
         """
 
         # Return a reprenting string
-        depth_str = '  ' * self._depth
+        prefix = '  ' * self._depth
         result = []
         for k, v in self.__dict__.items():
             if k.startswith('_'):
                 continue
             if k.endswith('_index'):
-                result.append(f'{depth_str}{colorama.Fore.WHITE}{colorama.Style.BRIGHT}{k}{colorama.Style.RESET_ALL} ({colorama.Fore.RED}{v}{colorama.Style.RESET_ALL}) -->')
+                result.append(f'{prefix}{colorama.Fore.WHITE}{colorama.Style.BRIGHT}{k}{colorama.Style.RESET_ALL} ({colorama.Fore.RED}{v}{colorama.Style.RESET_ALL}) -->')
                 other_obj = self._const_pool[v - 1]
                 other_obj._depth = self._depth + 1
                 result.append(f'{other_obj}')
             elif isinstance(v, list):
-                result.append(f'{depth_str}{colorama.Fore.WHITE}{colorama.Style.BRIGHT}{k}{colorama.Style.RESET_ALL}: [')
+                result.append(f'{prefix}{colorama.Fore.WHITE}{colorama.Style.BRIGHT}{k}{colorama.Style.RESET_ALL}: [')
                 for other_obj in v:
                     other_obj._depth = self._depth + 1
                     result.append(f'{other_obj}')
-                result.append(f'{depth_str}]')
+                result.append(f'{prefix}]')
             elif isinstance(v, bytes):
                 data = binascii.hexlify(v, ' ').decode()
-                result.append(f'{depth_str}{colorama.Fore.WHITE}{colorama.Style.BRIGHT}{k}{colorama.Style.RESET_ALL} = {colorama.Fore.RED}{data}{colorama.Style.RESET_ALL}')
+                result.append(f'{prefix}{colorama.Fore.WHITE}{colorama.Style.BRIGHT}{k}{colorama.Style.RESET_ALL} = {colorama.Fore.RED}{data}{colorama.Style.RESET_ALL}')
             elif isinstance(v, str):
-                result.append(f'{depth_str}{colorama.Fore.WHITE}{colorama.Style.BRIGHT}{k}{colorama.Style.RESET_ALL} = {colorama.Fore.LIGHTBLUE_EX}{v}{colorama.Style.RESET_ALL}')
+                fore_color = colorama.Fore.YELLOW + colorama.Style.DIM if k == 'tag' else colorama.Fore.LIGHTBLUE_EX
+                result.append(f'{prefix}{colorama.Fore.WHITE}{colorama.Style.BRIGHT}{k}{colorama.Style.RESET_ALL} = {fore_color}{v}{colorama.Style.RESET_ALL}')
             else:
-                result.append(f'{depth_str}{colorama.Fore.WHITE}{colorama.Style.BRIGHT}{k}{colorama.Style.RESET_ALL} = {colorama.Fore.GREEN}{v}{colorama.Style.RESET_ALL}')
+                result.append(f'{prefix}{colorama.Fore.WHITE}{colorama.Style.BRIGHT}{k}{colorama.Style.RESET_ALL} = {colorama.Fore.GREEN}{v}{colorama.Style.RESET_ALL}')
         return '\n'.join(result)
 
 class JavaClass(object):
@@ -107,18 +111,18 @@ class JavaClass(object):
         # Open the file for reading
         with open(self.file_path, 'rb') as fp:
 
-            # Parse the first part of the header
+            # Parse the header
             self.const_pool = []
-            self.header = JavaObject(self.const_pool)
-            hdr_magic, self.header.version_minor, self.header.major, const_pool_count = JavaClass._read_bytes_from_fp(fp, '>LHHH')
-            assert hdr_magic == 0xCAFEBABE, Exception(f'Invalid header magic {hdr_magic}')
+            self.header = JavaObject(self.const_pool, fp.tell())
+            self.header.magic, self.header.version_minor, self.header.major, const_pool_count = JavaClass._read_bytes_from_fp(fp, '>LHHH')
+            assert self.header.magic == 0xCAFEBABE, Exception(f'Invalid header magic {hdr_magic}')
             assert const_pool_count > 0, Exception(f'Invalid constant pool count {const_pool_count}')
             
             # Parse the constasnt pool
             for index in range(const_pool_count - 1):
 
                 # Save type and parse it
-                curr_obj = JavaObject(self.const_pool)
+                curr_obj = JavaObject(self.const_pool, fp.tell())
                 curr_obj.tag = JavaClass._read_bytes_from_fp(fp, '>B')
                 if curr_obj.tag == ConstType.CONSTANT_Class.value:
                     curr_obj.name_index = JavaClass._read_bytes_from_fp(fp, '>H')
@@ -150,9 +154,10 @@ class JavaClass(object):
 
                 # Add entry
                 self.const_pool.append(curr_obj)
-            
-            # Parse the second part of the header
-            self.header.access_flags, self.header.this_class_index, self.header.super_class_index, interfaces_count = JavaClass._read_bytes_from_fp(fp, '>HHHH')
+
+            # Parse the class descriptor
+            self.class_descriptor = JavaObject(self.const_pool, fp.tell())
+            self.class_descriptor.access_flags, self.class_descriptor.this_class_index, self.class_descriptor.super_class_index, interfaces_count = JavaClass._read_bytes_from_fp(fp, '>HHHH')
 
             # Save interfaces
             self.interfaces = []
@@ -163,7 +168,7 @@ class JavaClass(object):
             fields_count = JavaClass._read_bytes_from_fp(fp, '>H')
             self.fields = []
             for field_index in range(fields_count):
-                field = JavaObject(self.const_pool)
+                field = JavaObject(self.const_pool, fp.tell())
                 field.access_flags, field.name_index, field.descriptor_index = JavaClass._read_bytes_from_fp(fp, '>HHH')
                 field.attributes = self._parse_attributes(fp)
                 self.fields.append(field)
@@ -172,7 +177,7 @@ class JavaClass(object):
             methods_count = JavaClass._read_bytes_from_fp(fp, '>H')
             self.methods = []
             for method_index in range(methods_count):
-                method = JavaObject(self.const_pool)
+                method = JavaObject(self.const_pool, fp.tell())
                 method.access_flags, method.name_index, method.descriptor_index = JavaClass._read_bytes_from_fp(fp, '>HHH')
                 method.attributes = self._parse_attributes(fp)
                 self.methods.append(method)
@@ -191,7 +196,7 @@ class JavaClass(object):
         # Parse
         attributes = []
         for attr_index in range(attributes_count):
-            attr = JavaObject(self.const_pool)
+            attr = JavaObject(self.const_pool, fp.tell())
             attr.attribute_name_index, attr_len = JavaClass._read_bytes_from_fp(fp, '>HL')
             attr.data = fp.read(attr_len)
             attributes.append(attr)
@@ -227,6 +232,7 @@ def do_menu(jclass):
         print(f'{colorama.Fore.WHITE}{colorama.Style.BRIGHT}FILE{colorama.Style.RESET_ALL}: {colorama.Fore.GREEN}{os.path.basename(jclass.file_path)}{colorama.Style.RESET_ALL}')
         print(f'[{colorama.Fore.WHITE}{colorama.Style.BRIGHT}H{colorama.Style.RESET_ALL}]eader')
         print(f'[{colorama.Fore.WHITE}{colorama.Style.BRIGHT}C{colorama.Style.RESET_ALL}]onstant pool ({colorama.Fore.LIGHTBLUE_EX}{len(jclass.const_pool)}{colorama.Style.RESET_ALL})')
+        print(f'[{colorama.Fore.WHITE}{colorama.Style.BRIGHT}D{colorama.Style.RESET_ALL}]escriptor for the class')
         print(f'[{colorama.Fore.WHITE}{colorama.Style.BRIGHT}I{colorama.Style.RESET_ALL}nterfaces ({colorama.Fore.LIGHTBLUE_EX}{len(jclass.interfaces)}{colorama.Style.RESET_ALL})')
         print(f'[{colorama.Fore.WHITE}{colorama.Style.BRIGHT}F{colorama.Style.RESET_ALL}]ields ({colorama.Fore.LIGHTBLUE_EX}{len(jclass.fields)}{colorama.Style.RESET_ALL})')
         print(f'[{colorama.Fore.WHITE}{colorama.Style.BRIGHT}M{colorama.Style.RESET_ALL}]ethods ({colorama.Fore.LIGHTBLUE_EX}{len(jclass.methods)}{colorama.Style.RESET_ALL})')
@@ -253,6 +259,12 @@ def do_menu(jclass):
         elif choice == 'H':
             print(f'{colorama.Fore.CYAN}HEADER{colorama.Style.RESET_ALL}')
             print(jclass.header)
+            continue
+
+        # Print the class descriptor
+        elif choice == 'D':
+            print(f'{colorama.Fore.CYAN}CLASS DESCRIPTOR{colorama.Style.RESET_ALL}')
+            print(jclass.class_descriptor)
             continue
 
         # Maps choices to lists and use it to print list items
